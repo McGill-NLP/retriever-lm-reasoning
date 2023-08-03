1947b57d76189aa24b0164e21f68536991628732
Parishad BehnamGhader <parishad.behnamghader@mila.quebec>
2023-08-03 14:14:06 -0400

    Changed fid reporting format to jsonl

--- dpr/misc_utils.py
+++ dpr/misc_utils.py
@@ -1,25 +1,27 @@
+import json
 
-
-def save_lm_report(datas, retrieved_statements, predicted_alt, predicted_tokens_list, output_f=None):
+def save_lm_report(datas, retrieved_statements, predicted_alt, predicted_tokens_list, output_f=None):   
     for i, d in enumerate(datas):
-        output_f.write('Query: {}\n'.format(d['question']))
-        output_f.write('Retrieved: {}\n'.format(' | '.join(retrieved_statements[i])))
-        output_f.write('Generated: {}\n'.format(predicted_tokens_list[i]))
-        output_f.write('Alternatives: {}\n'.format(d['target']))
-        output_f.write(
-            '{} Preferred: {}\n'.format('+' if predicted_alt[i] == 0 else '-', d['target'][predicted_alt[i]]))
+        o = {}
+        o['query'] = d['question']
+        o['retrieved_statements'] = retrieved_statements[i]
+        o['alternatives'] = d['target']
+        o['ranked_target'] = d['target'][predicted_alt[i]]
+        o['ranked_correctly'] = True if predicted_alt[i] == 0 else False
+        o['predicted'] = predicted_tokens_list[i]
 
-        output_f.write('\n')
+        output_f.write(json.dumps(o) + '\n')
 
 
 def save_qa_report(datas, retrieved_statements, predicted_ans_list, output_f=None):
     for i, data in enumerate(datas):
-        output_f.write('Query: {}\n'.format(data['question']))
-        output_f.write('Retrieved: {}\n'.format(' | '.join(retrieved_statements[i])))
-        output_f.write('Expected: {}\n'.format(data['answer'][0]))
-        output_f.write('Generated: {}\n'.format(predicted_ans_list[i]))
+        o = {}
+        o['query'] = data['question']
+        o['retrieved_statements'] = retrieved_statements[i]
+        o['answer'] = data['answer'][0]
+        o['response'] = predicted_ans_list[i]
 
-        output_f.write('\n')
+        output_f.write(json.dumps(o) + '\n')
 
 
 def compute_f1_score(prediction, truth):
